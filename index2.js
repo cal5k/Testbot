4c9e904e3611723f1587f7b54e77bad601cc0f07
Dustin Walper <dustin.walper@gmail.com>
2016-06-09 14:49:07 -0400

    might work now

--- index2.js
+++ index2.js
@@ -32,7 +32,7 @@ if (!FB_PAGE_ID) {
   throw new Error('missing FB_PAGE_ID');
 }
 const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
-if (!FB_PAGE_TOKEN) {
+if (!FB_PAGE_TOKEN) 
   throw new Error('missing FB_PAGE_TOKEN');
 }
 const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
